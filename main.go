4f958f29150af61dabda4a6b7d3f30da2613efd1
Vincentius <vincentius85@gmail.com>
2026-02-16 13:55:46 +0700

    feat: adding logic and template for the process the order using waitgroup

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Person struct {
+	Name string
+	Wait int
+}
+
+func processOrder(p Person, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	time.Sleep(time.Duration(p.Wait) * time.Second)
+
+	if p.Name != "" {
+		fmt.Println("Menunggu antrian...")
+		fmt.Printf("Pesanan \"%s\" sudah siap\n", p.Name)
+	} else {
+		fmt.Println("Nama tidak boleh kosong")
+	}
+}
+
+func main() {
+	
+}
+
